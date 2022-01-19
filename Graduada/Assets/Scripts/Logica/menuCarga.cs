ee41eea29d6607daef2dea0f6d72bb0173ef4e18
LrRuben <38402745+LrRuben@users.noreply.github.com>
2022-01-19 22:29:02 +0100

    Pantalla Carga Final

--- Graduada/Assets/Scripts/Logica/menuCarga.cs
+++ Graduada/Assets/Scripts/Logica/menuCarga.cs
@@ -1,30 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class menuCarga : MonoBehaviour
 {
     [SerializeField] GameObject inventory;
-    //[SerializeField] GameObject spawner?;
+    public Sprite[] frames;
     private GameObject player;
+
+    [SerializeField] int fps;
+
+    public bool active;
     
     void Start()
     {
         player = GameObject.Find("Protagonista");
         player.GetComponent<IsometricPlayerMovement>().speed = 0;
+        active = true;
         StartCoroutine(load());
     }
 
     
     void Update()
     {
-        
+        if(active){
+            int index = (int)(Time.time * fps) % frames.Length;
+
+            GetComponent<Image>().sprite= frames[index];
+
+            //GetComponent<RawImage>().texture = frames[index];
+
+            //GetComponent<Material>().mainTexture = frames[index];
+
+            //GetComponent<Renderer>().material.mainTexture = frames[index];
+
+        }
     }
     
     private IEnumerator load(){
         yield return new WaitForSeconds(11);
         inventory.GetComponent<InventorySystem>().inventoryLoad();
         gameObject.SetActive(false);
+        active = false;
         player.GetComponent<IsometricPlayerMovement>().speed = 12;
     }
 }
+
